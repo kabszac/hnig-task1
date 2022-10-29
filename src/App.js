0e4303a5b178fd21aced1c6afc1ad771e183448f
Isaac Kabucho <kabszac@gmail.com>
2022-10-29 13:05:46 +0300

    Added link component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Links from './components/links/Links';
 import Profile from './components/profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Links/>
     </div>
   );
 }
---
+++ src/components/links/Links.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../button/Button';
+
+const Links = () => {
+    return (
+        <div>
+            <Button id='btn__twiiter' link='https://twitter.com/KabuchoIsaac/'>Kabucho Isaac</Button>
+            <Button id='btn__zuri' link='https://training.zuri.team/'>Zuri Team</Button>
+            <Button id='books' link='http://books.zuri.team '>Zuri Books</Button>
+            <Button id='book__python' link='https://books.zuri.team/python-for-beginners?ref_id=<kabuchokuria>,'>Python Books</Button>
+            <Button id='pitch' link='https://backgroundcheck.zuri.team'>Background Check for Coders </Button>
+            <Button id='book__design' link='https://books.zuri.team/design-rules'>Design Books</Button>
+
+        </div>
+    );
+};
+
+export default Links;
