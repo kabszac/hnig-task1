2b5c2887cb51568476d10d8b8fa3a14530bbeec9
Isaac Kabucho <kabszac@gmail.com>
2022-10-29 13:03:36 +0300

    Added the button links component

---
+++ src/components/button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({id, link, children}) => {
+    return (
+        <a id={id} href={link} className='button-link'> 
+            {children}
+        </a>
+    );
+};
+
+export default Button;
